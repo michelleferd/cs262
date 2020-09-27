96643dd9104d87a10595e37cffaf651637436758
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-09-26 21:01:58 -0400

    update to header.js

--- homework1/my-project/components/header.js
+++ homework1/my-project/components/header.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
-export default function Header() {
+export default function AddTodo({ submitHandler }) {
+  const [text, setText] = useState('');
+
+  const changeHandler = (val) => {
+    setText(val);
+  }
+
   return (
     <View>
-      <Text>My Todos</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='new todo...'
+        onChangeText={changeHandler}
+      />
+      <Button onPress={() => submitHandler(text)} title='add todo' color='coral' />
     </View>
   );
-};
+}
+
