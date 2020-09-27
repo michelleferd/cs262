095ddbc5a43756e333d3250a73ea036d797d7254
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-09-26 21:04:54 -0400

    Update todoItem.js

--- homework1/my-project/components/todoItem.js
+++ homework1/my-project/components/todoItem.js
@@ -6,8 +6,19 @@ export default function TodoItem({ pressHandler, item }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
       <View>
-        <Text>{item.text}</Text>
+        <Text style={styles.item}>{item.text}</Text>
       </View>
     </TouchableOpacity>
   )
-};
+}
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+  },
+});
