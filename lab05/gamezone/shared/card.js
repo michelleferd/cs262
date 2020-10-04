4be06cfdde85fb863a3bb0f8d3c323e7087d8dae
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-10-04 07:37:25 -0400

    Update card.js

--- lab05/gamezone/shared/card.js
+++ lab05/gamezone/shared/card.js
@@ -10,3 +10,19 @@ export default function Card(props) {
         </View>
     );
 }
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 6,
+        elevation: 3,
+        backgroundColor: '#fff',
+        shadowOffset: {width: 1, height: 1},
+        shadowColor: '#333',
+        shadowRadius: 2,
+        marginHorizontal: 4,
+        marginVertical: 6,
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 10,
+    }
+})
