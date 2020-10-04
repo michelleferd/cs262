2bb852b9221afb4e708f9a1be2808ae230c8d2c0
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-10-04 07:39:24 -0400

    Update header.js

--- lab05/gamezone/shared/header.js
+++ lab05/gamezone/shared/header.js
@@ -9,8 +9,29 @@ export default function Header({ navigation }) {
     }
 
     return (
-        <View>
+        <View style={StyleSheet.header}>
+            {/* */}
             <MaterialIcons name='menu' size={28} onPress={openMenu} />
+            {/* <View>
+                <Text style={StyleSheet.headerText}>{ title }</Text>
+            </View> */}
         </View>
     );
 };
+
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText:{
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#333',
+        letterSpacing: 1,
+    },
+})
