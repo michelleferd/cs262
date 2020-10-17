ce63cd9ddf2ee45892b4d0c6412d3ff9d0b40aea
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-10-17 14:56:09 -0400

    Update card.js
    update homework2

--- homework2/gamezone/shared/card.js
+++ homework2/gamezone/shared/card.js
@@ -10,3 +10,21 @@ export default function Card(props) {
         </View>
     );
 }
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 6,
+        elevation: 3,
+        backgroundColor: '#fff',
+        shadowOffset: {width: 1, height: 1,},
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+        marginHorizontal: 4,
+        marginVertical: 6,
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 10,
+    },
+});
+
