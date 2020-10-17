28481ebad749113c5873a0fd3521c0c8c4960865
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-10-17 14:57:53 -0400

    Update header.js
    update homework2

--- homework2/gamezone/shared/header.js
+++ homework2/gamezone/shared/header.js
@@ -14,3 +14,22 @@ export default function Header({ navigation }) {
         </View>
     );
 };
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#333',
+        letterSpacing: 1,
+    },
+    icon: {
+        position: 'absolute',
+        left: 16,
+    },
+})
