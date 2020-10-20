54ea0ae8bb53aa049269c0ae654bd99e72b62da6
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-10-20 11:57:33 -0400

    Update monopoly.sql

--- lab07/monopoly.sql
+++ lab07/monopoly.sql
@@ -9,6 +9,9 @@
 DROP TABLE IF EXISTS PlayerGame;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
+DROP TABLE IF EXISTS Property;
+DROP TABLE IF EXISTS House;
+DROP TABLE IF EXISTS Hotel;
 
 -- Create the schema.
 CREATE TABLE Game (
@@ -27,20 +30,41 @@ CREATE TABLE PlayerGame (
 	playerID integer REFERENCES Player(ID),
 	score integer
 	);
+	
+CREATE TABLE Hotel (
+	ID integer PRIMARY KEY
+	);
+
+CREATE TABLE House (
+	ID integer PRIMARY KEY
+	);
+
+CREATE TABLE Property (
+	ID integer PRIMARY KEY,
+	name varchar(50),
+	houseID integer REFERENCES House(ID),
+	hotelID integer REFERENCES Hotel(ID)
+	);
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
+GRANT SELECT ON Property TO PUBLIC;
+GRANT SELECT ON Hotel TO PUBLIC;
+GRANT SELECT ON House TO PUBLIC;
 
 -- Add sample records.
 INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
 INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
 INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
 
+INSERT INTO Game VALUES (4, '2006-06-29 19:43:00');
+
 INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
 INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
 INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
+INSERT INTO Player VALUES (4, 'mef25@students.calvin.edu', 'Michelle Ferdinands');
 
 INSERT INTO PlayerGame VALUES (1, 1, 0.00);
 INSERT INTO PlayerGame VALUES (1, 2, 0.00);
@@ -50,3 +74,11 @@ INSERT INTO PlayerGame VALUES (2, 2, 0.00);
 INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+
+
+INSERT INTO PlayerGame VALUES (4, 1, 0.00);
+
+
+INSERT INTO Property VALUES (1, 'Michigan');
+INSERT INTO Property VALUES (2, 'India');
+INSERT INTO Property VALUES (3, 'Pennsylvania');
