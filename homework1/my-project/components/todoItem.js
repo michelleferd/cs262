81688391dd04469ab354f26854456bf3963361f6
michelleferd <66312672+michelleferd@users.noreply.github.com>
2020-11-21 16:21:48 -0500

    lab11 changes

--- homework1/my-project/components/todoItem.js
+++ homework1/my-project/components/todoItem.js
@@ -1,6 +1,17 @@
+
+
+/**
+ * @author: chv5
+ */
+
 import React from 'react'
 import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+
+/**
+ * Default export function to navigation to TodoItem screen
+ * @param {presshandler} creates a button to navigate
+ */
 
 export default function TodoItem({ pressHandler, item }) {
   return (
